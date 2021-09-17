8b201696f474289a662e6a8fa94c4ef57e8092b8
Eduardo Breda <edu12561@hotmail.com>
2021-09-17 13:19:35 -0300

    create a validation to empty input

--- DOM-Project/projeto_inicial/listaDeTarefas.js
+++ DOM-Project/projeto_inicial/listaDeTarefas.js
@@ -6,6 +6,10 @@
     const input = document.querySelector("[data-form-input]");
     const valor = input.value;
 
+    const calendario = document.querySelector('[data-form-date]')
+    const data = moment(calendario.value)
+    console.log(data.format('DD/MM/YYYY'))
+
     const tarefa = document.createElement("li");
     tarefa.classList.add("task");
 
@@ -14,10 +18,13 @@
     tarefa.innerHTML = conteudo;
     tarefa.appendChild(BotaoConlui());
     tarefa.appendChild(BotaoDeleta());
-    lista.appendChild(tarefa);
-    input.value = "";
 
-    
+    if(input.value === '') {
+      window.alert('Por favor, insira um dado na lista')
+    } else {
+      lista.appendChild(tarefa);
+    }
+    input.value = "";
   };
 
   const novaTarefa = document.querySelector("[data-form-button]");
