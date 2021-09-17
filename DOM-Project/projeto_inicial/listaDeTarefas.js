1e8ea8311f247b84bb34b183a8b6d495f526e82a
Eduardo Breda <edu12561@hotmail.com>
2021-09-17 10:15:28 -0300

    create the initial files

---
+++ DOM-Project/projeto_inicial/listaDeTarefas.js
@@ -0,0 +1,63 @@
+(() => {
+  const criarTarefa = (evento) => {
+    evento.preventDefault();
+
+    const lista = document.querySelector("[data-list]");
+    const input = document.querySelector("[data-form-input]");
+    const valor = input.value;
+
+    const tarefa = document.createElement("li");
+    tarefa.classList.add("task");
+
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+    tarefa.appendChild(BotaoConlui());
+    tarefa.appendChild(BotaoDeleta());
+    lista.appendChild(tarefa);
+    input.value = "";
+
+    
+  };
+
+  const novaTarefa = document.querySelector("[data-form-button]");
+
+  novaTarefa.addEventListener("click", criarTarefa);
+
+  const BotaoConlui = () => {
+    const botaoConclui = document.createElement("button");
+
+    botaoConclui.classList.add("check-button");
+    botaoConclui.innerText = "concluir";
+
+    botaoConclui.addEventListener("click", concluirTarefa);
+
+    return botaoConclui;
+  };
+
+  const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target;
+
+    const tarefaCompleta = botaoConclui.parentElement;
+
+    tarefaCompleta.classList.toggle("done");
+  };
+
+  const BotaoDeleta = () => {
+    const botaoDeleta = document.createElement("button");
+    botaoDeleta.innerText = "deletar";
+    botaoDeleta.addEventListener("click", deletarTarefa);
+
+    return botaoDeleta;
+  };
+
+  const deletarTarefa = (evento) => {
+    const botaoDeleta = evento.target;
+    botaoDeleta.classList.add("remove-button");
+    const tarefaCompleta = botaoDeleta.parentElement;
+
+    tarefaCompleta.remove();
+
+    return botaoDeleta;
+  };
+})();
